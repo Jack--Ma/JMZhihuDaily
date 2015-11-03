fa058bf1d6d79e7f34ccf7e542e13cb1b170dc23
Jack--Ma <100858433@qq.com>
2015-11-03 15:12:07 +0800

    Initial Commit

---
+++ JMZhihuDaily/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JMZhihuDaily
+//
+//  Created by JackMa on 15/11/3.
+//  Copyright © 2015年 JackMa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JMZhihuDaily/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  JMZhihuDaily
+//
+//  Created by JackMa on 15/11/3.
+//  Copyright © 2015年 JackMa. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+  // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+  [super didReceiveMemoryWarning];
+  // Dispose of any resources that can be recreated.
+}
+
+@end
